1df91435631af3066d2957001ff0780a512bd272
Nicolas Bazille <oltodo@msn.com>
2024-06-09 18:58:14 +0200

    Start fetching data with GraphQL

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,7 @@
-import type { MetaFunction } from "@remix-run/node";
+import { json, type MetaFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { graphql } from "gql.tada";
+import client from "~/api.server";
 
 export const meta: MetaFunction = () => {
   return [
@@ -11,6 +14,31 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+const query = graphql(`
+  query GetEvents {
+    events {
+      documentId
+      title
+    }
+  }
+`);
+
+export const loader = async () => {
+  const { events } = await client.request(query);
+
+  return json({
+    events: events.filter(Boolean),
+  });
+};
+
 export default function Index() {
-  return null;
+  const { events } = useLoaderData<typeof loader>();
+
+  return (
+    <div className="container mx-auto">
+      {events.map((event) => (
+        <div key={event.documentId}>{event.title}</div>
+      ))}
+    </div>
+  );
 }
