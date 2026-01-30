34e2e6f674637a4d57483ca4edfabe3969c7779d
Nicolas Bazille <oltodo@msn.com>
2026-01-30 08:18:07 +0100

    Start designing

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,7 @@
+import { Brand } from "~/components/brand";
+import { Logo } from "~/components/logo";
+import { Button } from "~/components/ui/button";
+
 export function meta() {
   return [
     { title: "Agenda Chrétien" },
@@ -9,5 +13,21 @@ export function meta() {
 }
 
 export default function Home() {
-  return <div>Agenda Chrétien</div>;
+  return (
+    <div className="mx-auto flex max-w-5xl items-center justify-between p-4">
+      <div className="flex items-center gap-4">
+        <Logo className="h-10 fill-foreground" />
+        <div className="flex flex-col items-start">
+          <Brand className="h-5" />
+          <div className="mt-1 rounded-full rounded-tl-none bg-primary px-1.5 text-xs font-black text-background uppercase">
+            Grenoble
+          </div>
+        </div>
+      </div>
+
+      <Button size="sm" variant="outline">
+        Proposer un événement
+      </Button>
+    </div>
+  );
 }
